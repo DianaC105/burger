2d8b3b3487c723f281aefcdc513888fca0c92b30
Diana <carrillo.diana105@gmail.com>
2019-02-27 14:04:03 -0500

    starter files/ SQL

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Texas Style');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast Burger', true);
+INSERT INTO burgers (name) VALUES ('Portabello Burger');
